db331980b555bba9b3b91e09d0fd04c76c7a026e
nankaNULL <534129087@qq.com>
2019-04-12 17:54:23 +0800

    config配置

---
+++ config/config.default.js
@@ -0,0 +1,12 @@
+exports.keys = "aaa";
+exports.view = {
+  defaultViewEngine: 'nunjucks',
+  mapping: {
+    '.tpl': 'nunjucks'
+  }
+}
+exports.news = {
+  pageSize: 5,
+  // serverUrl: 'https://hacker-news.firebaseio.com/v0',
+  serverUrl: 'https://suggest.taobao.com/sug?code=utf-8&q=%E5%8D%AB%E8%A1%A3&callback=cb'
+}
