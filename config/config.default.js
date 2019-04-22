900d94070a90aeb19b289d6fc6029d71f48e7617
nankaNULL <534129087@qq.com>
2019-04-22 11:36:29 +0800

    中间件

--- config/config.default.js
+++ config/config.default.js
@@ -4,9 +4,12 @@ exports.view = {
   mapping: {
     '.tpl': 'nunjucks'
   }
-}
-exports.news = {
-  pageSize: 5,
-  // serverUrl: 'https://hacker-news.firebaseio.com/v0',
-  serverUrl: 'https://suggest.taobao.com/sug?code=utf-8&q=%E5%8D%AB%E8%A1%A3&callback=cb'
+};
+exports.middleware = [
+  'robot'
+];
+exports.robot = {
+  ua: [
+    /Baiduspider/i,
+  ]
 }
