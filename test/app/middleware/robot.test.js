070f90940c5c1546b1a5ae9b64c7e859815089d7
nankaNULL <534129087@qq.com>
2019-04-22 11:37:15 +0800

    单元测试

---
+++ test/app/middleware/robot.test.js
@@ -0,0 +1,9 @@
+const { app, mock, assert } = require('egg-mock/bootstrap');
+describe('text/app/middleware/robot.test.js', () => {
+  it('should block robot', () => {
+    return app.httpRequest()
+    .get('/')
+    .set('User-Agent', 'Baidus')
+    .expect(403);
+  })
+})
